78601492422282395dc983a74cdf6f9d54895823
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2023-06-22 09:36:03 +0800

    feat(core): add initTelecine

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,57 @@
-export function initTelecine() {
-  console.log("hello world");
+import { chromium } from "playwright";
+import {
+  InstallCaptionHelperConfig,
+  installCaptionHelper,
+} from "./installCaptionHelper";
+import { installMouseHelper } from "./installMouseHelper";
+
+export type initTelecineConfig = {
+  baseURL: string;
+  headless: boolean;
+  videoName: string;
+  videoPath: string;
+  size: {
+    width: number;
+    height: number;
+  };
+  captionConfig: InstallCaptionHelperConfig;
+};
+
+export async function initTelecine(config: initTelecineConfig) {
+  const { baseURL, headless, videoName, videoPath, size, captionConfig } =
+    config;
+
+  const browser = await chromium.launch({
+    headless,
+  });
+
+  const context = await browser.newContext({
+    recordVideo: {
+      dir: videoPath,
+      size: size,
+    },
+    viewport: size,
+    baseURL,
+  });
+
+  const page = await context.newPage();
+
+  await installCaptionHelper(page, captionConfig);
+  await installMouseHelper(page);
+
+  async function saveVideo() {
+    const video = page.video();
+
+    if (video) {
+      video.saveAs(`${videoPath}/${videoName}.webm`);
+      video.delete();
+    }
+  }
+
+  return {
+    context,
+    browser,
+    page,
+    saveVideo,
+  };
 }
